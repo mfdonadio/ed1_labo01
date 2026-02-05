a3791293ae052b659a7336f66c0f48871fccbcd1
Marco Donadio <marcodonadiocaste@gmail.com>
2026-02-05 08:05:09 -0600

    Generar arreglos

--- src/main/java/ed/lab/SortingTester.java
+++ src/main/java/ed/lab/SortingTester.java
@@ -31,6 +31,10 @@ public class SortingTester<T extends Comparable<T>> {
                 .average()
                 .orElse(0);
 
+        long sum = durations.stream()
+            .mapToLong(Duration::toMillis)
+            .sum();
+
         System.out.printf("\t\tTiempo promedio: %s ms\n", average);
     }
 }
